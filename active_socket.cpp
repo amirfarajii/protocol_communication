7d6188e4ba66eee644537c9dffe7a4a0affd666d
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2021-07-29 19:42:59 +0430

    active socket

---
+++ active_socket.cpp
@@ -0,0 +1,24 @@
+//create active socket
+#include<boost/asio.hpp>
+#include<iostream>
+
+using namespace boost;
+int main() {
+
+   asio::io_service ios;
+   asio::ip::tcp protocol = asio::ip::tcp::v4();
+   asio::ip::tcp::socket sock(ios);
+   system::error_code ec;
+   sock.open(protocol, ec);
+
+   if(ec.value() != 0){
+       std::cout<<"Failed, Error code: "<<
+       ec.value() <<". Massage: "<< ec.message();
+       return ec.value();
+   
+   }
+
+   return 0;
+
+
+}
